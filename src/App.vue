ddc15a0791694b1262a97f38f82c93be8add8e5e
Sebastian <sebastianlakla@gmail.com>
2025-11-23 17:46:02 +0100

    Add favicon

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
 <script setup></script>
 <template>
-  <p>Test test</p>
 </template>
 <style lang="scss">
-$color-main: rgb(57, 143, 0);
-p {
-  color: $color-main;
-}
-p{
-  color: red;
-}
 </style>
