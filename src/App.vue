25967924c81b810cbc8cf79ca8185eb45931f4da
Sebastian <sebastianlakla@gmail.com>
2025-11-29 20:41:49 +0100

    Add new files corresponding to fron and back cards.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
 import MainLayout from './components/MainLayout.vue';
-
 </script>
 <template>
   <MainLayout/>
 </template>
 <style lang="scss">
 @use './assets/sass/reset' as *;
-
 </style>
